9c29a0b9256af7ac967f07b35fe25eab5d193d34
Risabh Raj <Risabh.Raj@ril.com>
2018-01-10 10:41:53 +0530

    First working version [Organizes: pictures, software, zips and docs]

--- src/com/rizz/DOrganizer.java
+++ src/com/rizz/DOrganizer.java
@@ -6,6 +6,10 @@ public class DOrganizer {
 
     private static String downloadsPath = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads";
 
+    private static String[] pictureExtension = {".jpg", ".jpeg", ".png", ".gif"};
+    private static String[] softwareExtension = {".exe"};
+    private static String[] compressedFileExtension = {".zip", ".tgz", ".tar.gz", ".tar"};
+
     public static void main(String[] args) {
         //Checking if Downloads folder exists
         File downloadsFolder = new File(downloadsPath);
@@ -16,16 +20,57 @@ public class DOrganizer {
 
         //Creating directory structure inside downloads
         makeDirectory("Pictures");
+        makeDirectory("Software");
+        makeDirectory("CompressedFiles");
+        makeDirectory("Docs");
+
+        //Organizing downloads directory
+        organizeDir(pictureExtension, "Pictures");
+        organizeDir(softwareExtension, "Software");
+        organizeDir(compressedFileExtension, "CompressedFiles");
+
+        organizeDocuments("Docs");
     }
+
 
     private static void makeDirectory(String dirname) {
         File folder = new File(downloadsPath + "\\" + dirname);
-
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-//        boolean successful = folder.mkdir();
-//
-//        if(!successful || )
+        folder.mkdir();
     }
+
+
+    private static void organizeDir(String[] fileExtension, String destination) {
+        File downloadsFolder = new File(downloadsPath);
+        File[] listOfFiles = downloadsFolder.listFiles();
+
+        for (File f : listOfFiles) {
+            if (f.isFile()) {
+                String filename = f.getName();
+                for (String extension : fileExtension) {
+                    if (filename.endsWith(extension)) {
+                        f.renameTo(new File(downloadsPath + "\\" + destination + "\\" + filename));
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    private static void organizeDocuments(String destination) {
+        File downloadsFolder = new File(downloadsPath);
+        File[] listOfFiles = downloadsFolder.listFiles();
+
+        for (File f : listOfFiles) {
+            if (f.isFile()) {
+                String filename = f.getName();
+
+                f.renameTo(new File(downloadsPath + "\\" + destination + "\\" + filename));
+
+            }
+        }
+
+    }
+
 }
