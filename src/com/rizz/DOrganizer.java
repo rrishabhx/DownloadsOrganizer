0d59990372cec9377ef55f3c4bc7f4739a8acf32
Risabh Raj <ratedrrisabh@gmail.com>
2018-01-13 22:49:00 +0530

    Add functionality for Linux based OS

--- src/com/rizz/DOrganizer.java
+++ src/com/rizz/DOrganizer.java
@@ -4,7 +4,23 @@ import java.io.File;
 
 public class DOrganizer {
 
-    private static String downloadsPath = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads";
+    private static String myOS = findMyOS();
+    private static String downloadsPath;
+
+    static {
+        switch (myOS) {
+            case "Linux":
+                downloadsPath = System.getProperty("user.home") + "/Downloads";
+                break;
+            case "Windows":
+                downloadsPath = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads";
+                break;
+            default:
+                System.out.println("Sorry... This program currently supports Windows and Linux only.");
+                System.exit(1);
+                break;
+        }
+    }
 
     private static String[] pictureExtension = {".jpg", ".jpeg", ".png", ".gif"};
     private static String[] softwareExtension = {".exe"};
@@ -12,6 +28,8 @@ public class DOrganizer {
     private static String[] videoExtension = {".mp4", ".avi", ".flv", ".mkv", ".wmv"};
 
     public static void main(String[] args) {
+        //Printing OS
+        System.out.println("YOU ARE USING: " + myOS);
         //Checking if Downloads folder exists
         File downloadsFolder = new File(downloadsPath);
         if (!downloadsFolder.isDirectory()) {
@@ -41,8 +59,24 @@ public class DOrganizer {
     }
 
 
+    private static String findMyOS() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("linux")) {
+            return "Linux";
+        } else if (os.contains("window")) {
+            return "Windows";
+        }
+        return "What?";
+    }
+
+
     private static void makeDirectory(String dirname) {
-        File folder = new File(downloadsPath + "\\" + dirname);
+        File folder = null;
+        if (myOS.equals("Windows")) {
+            folder = new File(downloadsPath + "\\" + dirname);
+        } else if (myOS.equals("Linux")) {
+            folder = new File(downloadsPath + "/" + dirname);
+        }
         if (!folder.mkdir() && !folder.exists()) {
             System.err.println("Unable to create " + dirname + " directory.");
         }
@@ -52,18 +86,28 @@ public class DOrganizer {
     private static void organizeDir(String[] fileExtension, String destination) {
         File downloadsFolder = new File(downloadsPath);
         File[] listOfFiles = downloadsFolder.listFiles();
+        String destinationPath = null;
+
+        if (myOS.equals("Windows")) {
+            destinationPath = downloadsPath + "\\" + destination + "\\";
+        } else if (myOS.equals("Linux")) {
+            destinationPath = downloadsPath + "/" + destination + "/";
+        }
 
         for (File f : listOfFiles) {
-            if (f.isFile()) {
-                String filename = f.getName();
-                for (String extension : fileExtension) {
-                    if (filename.endsWith(extension)) {
-                        if (!f.renameTo(new File(downloadsPath + "\\" + destination + "\\" + filename))) {
-                            System.err.println("Unable to move " + filename + " to " + destination);
-                        }
+            if (!f.isFile()) {
+                continue;
+            }
+
+            String filename = f.getName();
+            for (String extension : fileExtension) {
+                if (filename.endsWith(extension)) {
+                    if (!f.renameTo(new File(destinationPath + filename))) {
+                        System.err.println("Unable to move " + filename + " to " + destination);
                     }
                 }
             }
+
 
         }
 
@@ -73,16 +117,25 @@ public class DOrganizer {
     private static void organizeDocuments() {
         File downloadsFolder = new File(downloadsPath);
         File[] listOfFiles = downloadsFolder.listFiles();
+        String destinationPath = null;
+
+        if (myOS.equals("Windows")) {
+            destinationPath = downloadsPath + "\\" + "Docs" + "\\";
+        } else if (myOS.equals("Linux")) {
+            destinationPath = downloadsPath + "/" + "Docs" + "/";
+        }
+
 
         for (File f : listOfFiles) {
-            if (f.isFile()) {
-                String filename = f.getName();
-
-                if (!f.renameTo(new File(downloadsPath + "\\" + "Docs" + "\\" + filename))) {
-                    System.err.println("Unable to move " + filename + " to " + "Docs");
-                }
-
+            if (!f.isFile()) {
+                continue;
             }
+            String filename = f.getName();
+
+            if (!f.renameTo(new File(destinationPath + filename))) {
+                System.err.println("Unable to move " + filename + " to " + "Docs");
+            }
+
         }
 
     }
