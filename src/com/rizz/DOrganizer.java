b142ad8b483d6143578ff7e3d90fd2a582be1dd8
Risabh Raj <ratedrrisabh@gmail.com>
2018-01-22 22:28:41 +0530

    Buggy logic to organize directories

--- src/com/rizz/DOrganizer.java
+++ src/com/rizz/DOrganizer.java
@@ -1,5 +1,7 @@
 package com.rizz;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -7,22 +9,27 @@ import java.util.HashSet;
 
 public class DOrganizer {
 
-    private static final HashMap<String, HashSet<String>> fileTypeMap = new HashMap<>();
     private static String myOS = findMyOS();
     private static String downloadsPath;
+    private static String downloadsPathString;
     private static HashSet<String> pictureExtensionSet = new HashSet<>(Arrays.asList(".jpg", ".jpeg", ".png", ".gif", ".bmp"));
     private static HashSet<String> softwareExtensionSet = new HashSet<>(Arrays.asList(".exe", ".jar", ".bat", ".py", ".sh", ".pl", ".msi", ".ini", ".run", ".out", ".deb", ".rpm"));
     private static HashSet<String> archiveExtensionSet = new HashSet<>(Arrays.asList(".zip", ".tgz", ".tar.gz", ".tar", ".bz2", ".7z", ".rar", ".pkg"));
     private static HashSet<String> videoExtensionSet = new HashSet<>(Arrays.asList(".mp4", ".avi", ".flv", ".mkv", ".wmv", ".3gp", ".mpeg", ".mpg", ".h264"));
     private static HashSet<String> audioExtensionSet = new HashSet<>(Arrays.asList(".mp3", ".wav", ".aif", ".mpa", ".wma", ".wpl"));
 
+    private static HashMap<String, HashSet<String>> fileTypeMap = new HashMap<>();
+
+
     static {
         switch (myOS) {
             case "Linux":
                 downloadsPath = System.getProperty("user.home") + "/Downloads";
+                downloadsPathString = System.getProperty("user.home") + "/Downloads/";
                 break;
             case "Windows":
                 downloadsPath = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads";
+                downloadsPathString = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads\\";
                 break;
             default:
                 System.out.println("xxx Sorry... This program currently supports Windows and Linux only. xxx");
@@ -58,7 +65,7 @@ public class DOrganizer {
         makeDirectory("Audio");
 
         //Organizing downloads directory
-        System.out.println("\n>>> Organizing folders:~ ");
+        System.out.println("\n>>> Organizing files:~ ");
         runOrganizer();
 
         System.out.println("\n>>> \"" + downloadsPath + "\" ORGANIZED <<<\n");
@@ -93,85 +100,47 @@ public class DOrganizer {
     private static void runOrganizer() {
         File downloadsFolder = new File(downloadsPath);
         File[] listOfFiles = downloadsFolder.listFiles();
-
-
-        for (File f : listOfFiles) {
-            String filename = f.getName();
-        }
-
-    }
-
-    private static void organizeDir(String[] fileExtension, String destination) {
-        File downloadsFolder = new File(downloadsPath);
-        File[] listOfFiles = downloadsFolder.listFiles();
-        String destinationPath = null;
-        boolean fileMoveStatus = true;
-
-        if (myOS.equals("Windows")) {
-            destinationPath = downloadsPath + "\\" + destination + "\\";
-        } else if (myOS.equals("Linux")) {
-            destinationPath = downloadsPath + "/" + destination + "/";
-        }
-
-        for (File f : listOfFiles) {
-            if (!f.isFile()) {
-                continue;
-            }
-
-            String filename = f.getName();
-            for (String extension : fileExtension) {
-                if (filename.endsWith(extension)) {
-                    if (!f.renameTo(new File(destinationPath + filename))) {
-                        //Checking if the file is not DownloadsOrganizer executable
-                        if (!filename.equals("DownloadsOrganizer.jar")) {
-                            System.out.println("xxx Unable to move " + filename + " to " + destination + " xxx");
-                            fileMoveStatus = false;
-                        }
-                    }
-                }
-            }
-
-        }
-
-
-        System.out.println(">>> " + destination + ": " + (fileMoveStatus ? "SUCCESS" : "FAILURE"));
-
-    }
-
-
-    private static void organizeDocuments() {
-        File downloadsFolder = new File(downloadsPath);
-        File[] listOfFiles = downloadsFolder.listFiles();
-        String destinationPath = null;
-        boolean fileMoveStatus = true;
-
-        if (myOS.equals("Windows")) {
-            destinationPath = downloadsPath + "\\" + "Docs" + "\\";
-        } else if (myOS.equals("Linux")) {
-            destinationPath = downloadsPath + "/" + "Docs" + "/";
-        }
+        boolean isDocumentFile = true;
+        boolean moveStatus = false;
 
 
         for (File f : listOfFiles) {
             if (!f.isFile()) {
                 continue;
             }
+
             String filename = f.getName();
+            String fileExtension = FilenameUtils.getExtension(filename);
 
-            if (!f.renameTo(new File(destinationPath + filename))) {
-                //Checking if the file is not DownloadsOrganizer executable
-                if (!filename.equals("DownloadsOrganizer.jar")) {
-                    System.out.println("xxx Unable to move " + filename + " to " + "Docs. xxx");
-                    fileMoveStatus = false;
+            for (String type : fileTypeMap.keySet()) {
+                if (fileTypeMap.get(type).contains(fileExtension)) {
+                    String destinationPath = downloadsPathString + type;
+                    moveStatus = moveFile(destinationPath, f);
+                    isDocumentFile = false;
+                    break;
                 }
-
+            }
+            if (isDocumentFile) {
+                moveStatus = moveFile("Docs", f);
             }
 
+            if (moveStatus) {
+                System.out.println("\t * " + filename);
+            }
         }
 
-        System.out.println(">>> " + "Docs" + ": " + (fileMoveStatus ? "SUCCESS" : "FAILURE"));
+    }
 
 
+    private static boolean moveFile(String destinationPath, File filename) {
+        if (!filename.renameTo(new File(destinationPath))) {
+            //Checking if the file is not DownloadsOrganizer executable
+            if (!filename.getName().equals("DownloadsOrganizer.jar")) {
+                System.err.println("xxx Unable to move " + filename + " to " + destinationPath + " xxx");
+                return false;
+            }
+        }
+        return true;
     }
 
 }
