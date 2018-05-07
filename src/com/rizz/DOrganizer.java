cd054acf36e1961ca7837004fe8ad68e3e445f4f
Risabh Raj <Risabh.Raj@ril.com>
2018-05-07 15:54:55 +0530

    Implement logging
    Now, program will log each execution in (jarLocation)/.dOlogs/ hidden folder.
    Although,logger adds overhead, but it will come handy in failure cases.

--- src/com/rizz/DOrganizer.java
+++ src/com/rizz/DOrganizer.java
@@ -1,44 +1,33 @@
 package com.rizz;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class DOrganizer {
 
-    private static String myOS = findMyOS();
+    private static String downloadsDir;
     private static String downloadsPath;
-    private static String downloadsPathString;
-    private static String SLASH;
+    private static String FS;
     private static HashSet<String> pictureExtensionSet = new HashSet<>(Arrays.asList("jpg", "jpeg", "png", "gif", "bmp"));
     private static HashSet<String> softwareExtensionSet = new HashSet<>(Arrays.asList("exe", "jar", "bat", "py", "sh", "pl", "msi", "ini", "run", "out", "deb", "rpm"));
     private static HashSet<String> archiveExtensionSet = new HashSet<>(Arrays.asList("zip", "tgz", "gz", "tar", "bz2", "7z", "rar", "pkg"));
     private static HashSet<String> videoExtensionSet = new HashSet<>(Arrays.asList("mp4", "avi", "flv", "mkv", "wmv", "3gp", "mpeg", "mpg", "h264"));
     private static HashSet<String> audioExtensionSet = new HashSet<>(Arrays.asList("mp3", "wav", "aif", "mpa", "wma", "wpl"));
-
     private static HashMap<String, HashSet<String>> fileTypeMap = new HashMap<>();
-
+    private static Logger logger = LogManager.getLogger(DOrganizer.class);
 
     static {
-        switch (myOS) {
-            case "Linux":
-                downloadsPath = System.getProperty("user.home") + "/Downloads";
-                downloadsPathString = System.getProperty("user.home") + "/Downloads/";
-                SLASH = "/";
-                break;
-            case "Windows":
-                downloadsPath = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads";
-                downloadsPathString = "C:\\Users\\" + System.getProperty("user.name") + "\\Downloads\\";
-                SLASH = "\\";
-                break;
-            default:
-                System.out.println("xxx Sorry... This program currently supports Windows and Linux only. xxx");
-                System.exit(1);
-                break;
-        }
+        FS = File.separator;
+        String HOME = System.getProperty("user.home");
+        downloadsDir = HOME + FS + "Downloads";
+        downloadsPath = downloadsDir + FS;
 
         //Populating fileTypeMap
         fileTypeMap.put("Pictures", pictureExtensionSet);
@@ -49,17 +38,19 @@ public class DOrganizer {
     }
 
     public static void main(String[] args) {
-        System.out.println(">>> Going to organize: \"" + downloadsPath + "\"");
+        logger.info("******************** " + new Date() + " ********************");
+        logger.info("Going to organize: [" + downloadsDir + "]");
         //Printing OS
-        System.out.println(">>> Operating System: " + myOS);
+        logger.info("Operating System: " + System.getProperty("os.name"));
+
         //Checking if Downloads folder exists
-        File downloadsFolder = new File(downloadsPath);
+        File downloadsFolder = new File(downloadsDir);
         if (!downloadsFolder.isDirectory()) {
-            System.err.println("xxx Downloads directory doesn't exist xxx");
+            logger.error("Downloads directory doesn't exist. Exiting....");
             System.exit(1);
         }
 
-        //Creating directory structure inside downloads
+        //Creating file-specific directory structure inside downloads
         makeDirectory("Pictures");
         makeDirectory("Software");
         makeDirectory("Archives");
@@ -68,49 +59,33 @@ public class DOrganizer {
         makeDirectory("Audios");
 
         //Organizing downloads directory
-        System.out.println("\n>>> Organizing files:~ ");
+        logger.info("Organizing files:~ ");
         runOrganizer();
 
-        System.out.println("\n>>> \"" + downloadsPath + "\" ORGANIZED <<<\n");
+        logger.info(">>>>>>>>>>>>>>>>>>>> [" + downloadsDir + "] ORGANIZED <<<<<<<<<<<<<<<<<<<<");
 
-    }
-
-
-    private static String findMyOS() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("linux")) {
-            return "Linux";
-        } else if (os.contains("window")) {
-            return "Windows";
-        }
-        return "What?";
     }
 
 
     private static void makeDirectory(String dirname) {
-        File folder = null;
-        if (myOS.equals("Windows")) {
-            folder = new File(downloadsPath + "\\" + dirname);
-        } else if (myOS.equals("Linux")) {
-            folder = new File(downloadsPath + "/" + dirname);
-        }
+        File folder = new File(downloadsPath + dirname);
+
         if (!folder.mkdir() && !folder.exists()) {
-            System.err.println("xxx Unable to create " + dirname + " directory. xxx");
+            logger.error("xxx Unable to create " + dirname + " directory. xxx");
         }
     }
 
 
     private static void runOrganizer() {
-        File downloadsFolder = new File(downloadsPath);
+        File downloadsFolder = new File(downloadsDir);
         File[] listOfFiles = downloadsFolder.listFiles();
 
 
         for (File f : listOfFiles) {
             boolean isDocumentFile = true;
-            boolean moveStatus = false;
 
-            //Checking if it's a folder
-            if (!f.isFile()) {
+            //Skip if 'f' is folder/DownloadsOrganizer.jar
+            if (f.isDirectory() || f.getName().equals("DownloadsOrganizer.jar")) {
                 continue;
             }
 
@@ -119,34 +94,32 @@ public class DOrganizer {
 
             for (String fileTypeFolder : fileTypeMap.keySet()) {
                 if (fileTypeMap.get(fileTypeFolder).contains(fileExtension)) {
-                    String absoluteFilePath = downloadsPathString + fileTypeFolder + SLASH + filename;
-                    moveStatus = moveFile(absoluteFilePath, f);
+                    String absoluteFilePath = downloadsPath + fileTypeFolder + FS + filename;
+                    moveFile(absoluteFilePath, f);
                     isDocumentFile = false;
                     break;
                 }
             }
             if (isDocumentFile) {
-                String absoluteDocumentPath = downloadsPathString + "Docs" + SLASH + filename;
-                moveStatus = moveFile(absoluteDocumentPath, f);
+                String absoluteDocumentPath = downloadsPath + "Docs" + FS + filename;
+                moveFile(absoluteDocumentPath, f);
             }
 
-            if (moveStatus && !f.getName().equals("DownloadsOrganizer.jar")) {
-                System.out.println("\t * " + filename);
-            }
         }
 
     }
 
 
-    private static boolean moveFile(String absoluteFilePath, File file) {
+    private static void moveFile(String absoluteFilePath, File file) {
         if (!file.renameTo(new File(absoluteFilePath))) {
+
             //Checking if the file is not DownloadsOrganizer executable
             if (!file.getName().equals("DownloadsOrganizer.jar")) {
-                System.err.println("xxx Unable to move " + file + " to " + absoluteFilePath + " xxx");
-                return false;
+                logger.info("FAILURE: " + file.getName() + " --> " + absoluteFilePath);
+                return;
             }
         }
-        return true;
+        logger.info("SUCCESS: " + file.getName() + " --> " + absoluteFilePath);
     }
 
 }
